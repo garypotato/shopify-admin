5fd0c5479d864c060e301c478d46fe746eca18fa
garyc <garypotato666@gmail.com>
2025-01-10 19:58:01 +1100

    remove console.log

--- app/api/shopify/route.js
+++ app/api/shopify/route.js
@@ -11,15 +11,12 @@ export async function GET() {
 
 	try {
 		products = await shopify.product.list();
-		console.log('Products fetched:', products);
 
 		return new Response(JSON.stringify({ data: products, error: null }), {
 			status: 200,
 			headers: { 'Content-Type': 'application/json' },
 		});
 	} catch (error) {
-		console.error('Error fetching products:', error);
-
 		return new Response(JSON.stringify({ data: null, error: error.message }), {
 			status: 500,
 			headers: { 'Content-Type': 'application/json' },
